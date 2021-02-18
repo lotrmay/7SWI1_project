4a9fcc230f4d2472c771b1acc7ffe2186d91c421
Lukas <lukasr00@seznam.cz>
2021-02-18 20:53:09 +0100

    Redesign of mainForm

--- carservice_7swi1/src/cz/osu/carservice/controllers/MainFormController.java
+++ carservice_7swi1/src/cz/osu/carservice/controllers/MainFormController.java
@@ -5,14 +5,29 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import java.awt.*;
+import java.net.URI;
 
 public class MainFormController {
+    //region ElementsVariable
     @FXML
-    public Button closeBtn;
+    public ImageView closeBtn;
+    @FXML
+    public ImageView facebookBtn;
+    @FXML
+    public ImageView instagramBtn;
+    @FXML
+    public ImageView twitterBtn;
     @FXML
     public Button createOrderBtn;
     @FXML
     public Button orderListBtn;
+    @FXML
+    public Label helpBtn;
+    //endregion
 
     private final DatabaseManager databaseManager;
 
@@ -21,16 +36,46 @@ public class MainFormController {
     }
 
     @FXML
-    private void closeApplication(ActionEvent event){
+    private void closeApplication(MouseEvent event){
         Platform.exit();
         System.exit(0);
     }
-
+    @FXML
+    private void runFacebook(MouseEvent event){
+        try {
+            Desktop.getDesktop().browse(new URI("https://www.facebook.com/"));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+    @FXML
+    private void runInstagram(MouseEvent event){
+        try {
+            Desktop.getDesktop().browse(new URI("https://www.instagram.com/"));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+    @FXML
+    private void runTwitter(MouseEvent event){
+        try {
+            Desktop.getDesktop().browse(new URI("https://twitter.com/"));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+    @FXML
+    private void runHelpSite(MouseEvent event){
+        try {
+            Desktop.getDesktop().browse(new URI("https://seznam.cz/"));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
     @FXML
     private void showOrderForm(ActionEvent event){
         System.out.println(databaseManager.testSelect());
     }
-
     @FXML
     private void showOrderListForm(ActionEvent event){
         databaseManager.testInsert("Pepa","Zelený");
